5688fbc5d77e2bd46293f1e5135942fc37f32484
PranjeetGoswami <prgoswami2020@gmail.com>
2025-03-18 02:03:03 +0530

    fixes api issues

--- src/components/Popular.tsx
+++ src/components/Popular.tsx
@@ -28,7 +28,7 @@ const PopularList = async ({ popular }: PopularListProps) => {
         <div className="text-white">
             <div className="max-w-5xl mx-auto p-4">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    {popular.data.map((item) => {
+                    {popular?.data?.map((item) => {
                         const { id, Title, Date: headerDate, PopularImage } = item;
 
                         const formattedDate = new Date(headerDate).toLocaleDateString(
